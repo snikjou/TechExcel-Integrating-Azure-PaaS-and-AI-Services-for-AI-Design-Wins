2b7f4af7da9a4b3751ab303299e62ce2e6b4d64d
Said Nikjou <44277963+snikjou@users.noreply.github.com>
2025-01-16 23:27:08 +0000

    API

--- src/data/AddManagedIdentityUser.sql
+++ src/data/AddManagedIdentityUser.sql
@@ -1,4 +1,4 @@
-CREATE USER TechExcelMI FROM EXTERNAL PROVIDER;
-ALTER ROLE db_datareader ADD MEMBER TechExcelMI;
-ALTER ROLE db_datawriter ADD MEMBER TechExcelMI;
+CREATE USER "id-4j3fznwvxqumg" FROM EXTERNAL PROVIDER;
+ALTER ROLE db_datareader ADD MEMBER "id-4j3fznwvxqumg";
+ALTER ROLE db_datawriter ADD MEMBER "id-4j3fznwvxqumg";
 
